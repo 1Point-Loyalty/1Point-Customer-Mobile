c2fa84f059bd2afe8a9eb0a37b57a2a2ccfd54fa
Saif Abuosba <48367948+SaifA21@users.noreply.github.com>
2024-12-28 14:26:02 -0500

    in progress

--- app/(auth)/(tabs)/qr.tsx
+++ app/(auth)/(tabs)/qr.tsx
@@ -1,269 +1,281 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { View, Text, StyleSheet, Image, SafeAreaView } from 'react-native';
-import PagerView from 'react-native-pager-view';
-import QRCode from 'react-native-qrcode-svg';
-
+import React, { useEffect, useRef, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import auth from "@react-native-firebase/auth";
+import QRCode from "react-native-qrcode-svg";
 
 export default function TabTwoScreen() {
-
   const userData = {
-    firstName: 'Aryan',
-    lastName: 'Abuosba',
-    email: 'contact.saifa@gmail.com',
-    phoneNumber: '416-899-6360',
-    created_time: '2024-09-21 22:26:35',
-    updated_time: '2024-09-21 22:26:35',
-    is_BO: false,
-    ipAddress: '99.233.13.66'
+    firstName: "Saif",
+    lastName: "Abuosba",
+    phoneNumber: "555-555-5555",
   };
 
+  const [encryptedQRCode, setEncryptedQRCode] = useState("INVALID");
+  const fetchData = async () => {
+    const user = auth().currentUser;
+    const userId = user?.uid;
 
-  // call lambda function to get user data
-  // const [userData, setUserData] = useState(null);
-  // useEffect(() => {
-  //   fetch('https://api.example.com/user')
-  //     .then(response => response.json())
-  //     .then(data => setUserData(data));
-  // }, []);
+    const token = await user?.getIdToken();
 
-
-  // extract user phone number from user data
-  // const userPhoneNumber = userData?.phoneNumber;
-
-  // display qr code for user phone number
-  // return (
-  //   <SafeAreaView style={styles.main}>
-  //     <View style={styles.container}>
-  //       <View style={styles.header}>
-  //         <Image
-  //           source={require('@/assets/images/1Point_Logo.png')}
-  //           style={styles.headerImage}
-  //         />
-  //         <View style={styles.headerText}>
-  //           <Text style={styles.welcomeText}>Welcome John!</Text>
-  //         </View>
-  //       </View>
-  //     </View>  
-  //   </SafeAreaView >
-  // );
-
-
-  // Render the points section
-  const renderPointsSection = () => {
-    return (
-
-      <View style={styles.pointsSection}>
-
-        <View style={styles.row}>
-
-          <Text style={styles.label}>Current Point Balance:</Text>
-
-          <View style={styles.pointContainer}>
-
-            <Image
-              source={require('@/assets/images/1Point_Logo.png')}
-              style={styles.pointAmounts}
-            />
-            <Text style={styles.pointText}>1,978</Text>
-
-          </View>
-
-        </View>
-
-        <View style={styles.row}>
-
-          <Text style={styles.label}>Total Points Collected:</Text>
-
-          <View style={styles.pointContainer}>
-
-            <Image
-              source={require('@/assets/images/1Point_Logo.png')}
-              style={styles.pointAmounts}
-            />
-            <Text style={styles.pointText}>34,909</Text>
-
-          </View>
-
-        </View>
-
-        <View style={styles.row}>
-
-          <Text style={styles.label}>Last Transaction:</Text>
-          <Text style={styles.transactionText}>July 7th, 2024</Text>
-
-        </View>
-
-      </View>
-    );
+    fetch(`https://admin.1-point.ca/api/getEncryptedUserDetails/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setEncryptedQRCode(data);
+        alert(`Response: ${JSON.stringify(data)}`);
+      })
+      .catch((error) => {
+        alert(`Error: ${error.message}`);
+      });
   };
 
-  // Render the home screen 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // Render the qr page
   return (
     <SafeAreaView style={styles.main}>
-
       <View style={styles.container}>
-
         <View style={styles.header}>
           <Image
-            source={require('@/assets/images/1Point_Logo.png')}
+            source={require("@/assets/images/1Point_Logo.png")}
             style={styles.headerImage}
           />
           <View style={styles.headerText}>
-            <Text style={styles.welcomeText}>Welcome {userData.firstName}</Text>
+            <Text style={styles.titleText}>WALLET</Text>
           </View>
         </View>
 
-        <View style={styles.qrCode}>
-          <QRCode
-            value={userData.phoneNumber}
-            size={150}
-            color="black" />
+        <View style={styles.qrCodeContainer}>
+          <View style={styles.qrCode}>
+            <QRCode
+              backgroundColor="transparent"
+              value={encryptedQRCode}
+              size={120}
+              color="white"
+            />
+          </View>
+          <View style={styles.qrCodeText}>
+            <Text
+              style={{
+                color: "white",
+                fontFamily: "Inter",
+                fontSize: 26,
+                fontWeight: "bold",
+              }}
+            >
+              {userData.firstName.toUpperCase()}
+            </Text>
+            <Text
+              style={{
+                color: "white",
+                fontFamily: "Inter",
+                fontSize: 26,
+                fontWeight: "bold",
+              }}
+            >
+              {userData.lastName.toUpperCase()}
+            </Text>
+            <Text> </Text>
+            <Text
+              style={{
+                color: "white",
+                fontFamily: "Inter",
+                fontSize: 14,
+                fontWeight: "bold",
+              }}
+            >
+              MEMBER SINCE: 11/24
+            </Text>
+          </View>
         </View>
-
-
-        <View>
-          <Text style={styles.qrText}>{userData.phoneNumber}</Text>
-        </View>
-
-        <View>
-          <Text style={styles.qrText} >Scan the QR code to earn/redeem points!</Text>
-        </View>
-
-
       </View>
-
-
 
       <View style={styles.lower}>
-        {renderPointsSection()}
-      </View>
+        <View style={styles.pointsSection}>
+          <View style={styles.row}>
+            <Text style={styles.label}>Current Point Balance:</Text>
 
-    </SafeAreaView >
+            <View style={styles.pointContainer}>
+              <Image
+                source={require("@/assets/images/1Point_Logo.png")}
+                style={styles.pointAmounts}
+              />
+              <Text style={styles.pointText}>1,978</Text>
+            </View>
+          </View>
+
+          <View style={styles.row}>
+            <Text style={styles.label}>Total Points Collected:</Text>
+
+            <View style={styles.pointContainer}>
+              <Image
+                source={require("@/assets/images/1Point_Logo.png")}
+                style={styles.pointAmounts}
+              />
+              <Text style={styles.pointText}>34,909</Text>
+            </View>
+          </View>
+
+          <View style={styles.row}>
+            <Text style={styles.label}>Last Transaction:</Text>
+            <Text style={styles.transactionText}>July 7th, 2024</Text>
+          </View>
+
+          <View style={styles.loginContainer}>
+            <TouchableOpacity
+              // fetch the encrypted QR code
+              onPress={fetchData}
+              style={styles.loginButton}
+            >
+              <Text style={styles.loginButtonText}>Refresh</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   main: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   container: {
     flex: 1,
     padding: 15,
     paddingTop: 40,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   headerText: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginRight: 71,
   },
-  qrCode: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 10,
-    borderRadius: 32,
-    justifyContent: 'center',
+  qrCodeContainer: {
+    backgroundColor: "#242121",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 30,
+    borderRadius: 25,
+    justifyContent: "flex-start",
     margin: 5,
-    position: 'relative',
+    position: "relative",
+  },
+  qrCode: {
+    flexDirection: "column",
+    padding: 15,
+    justifyContent: "flex-start",
+  },
+  qrCodeText: {
+    flexDirection: "column",
+    padding: 5,
+    justifyContent: "flex-start",
   },
   lower: {
     flex: 1,
-    paddingTop: 200,
+    paddingTop: 100,
     borderRadius: 32,
-    backgroundColor: '#ggg',
-    justifyContent: 'flex-end',
+    backgroundColor: "#ggg",
+    justifyContent: "flex-end",
   },
   header: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 20,
   },
-  welcomeText: {
-    fontSize: 24,
-    fontWeight: 'bold',
+  titleText: {
+    fontSize: 30,
+    fontWeight: "bold",
   },
-  qrText: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  newSection: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#eee',
-    padding: 10,
-    borderRadius: 32,
-    margin: 5,
-    position: 'relative',
-  },
+
   headerImage: {
     width: 71,
     height: 71,
     marginRight: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   pointAmounts: {
     width: 46,
     height: 46,
     marginRight: 10,
   },
-  newBrandLogo: {
-    width: 120,
-    height: 120,
-    marginRight: 10,
-    borderRadius: 10,
-    margin: 5,
-  },
-  newLabelContainer: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-    backgroundColor: 'black',
-    borderRadius: 32,
-  },
   pointsSection: {
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
     padding: 15,
     paddingBottom: 50,
     borderTopLeftRadius: 32,
     borderTopRightRadius: 32,
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: '#fff',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
     padding: 15,
     borderRadius: 32,
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 10,
     marginVertical: 24,
-    minHeight: 75
+    minHeight: 75,
   },
   pointContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   pointText: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   label: {
     fontSize: 16,
-    fontWeight: 'bold'
-  },
-  transactionRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    fontWeight: "bold",
   },
   transactionText: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+  },
+  loginText: {
+    marginBottom: 10,
+    marginTop: 20,
+  },
+  loginButton: {
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "black",
+    borderRadius: 10,
+    width: "100%",
+    alignItems: "center",
+  },
+  loginButtonText: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  loginContainer: {
+    alignItems: "center",
+    width: "100%",
+    marginTop: 20,
   },
 });
--- app/(auth)/(tabs)/settings.tsx
+++ app/(auth)/(tabs)/settings.tsx
@@ -1,15 +1,20 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { View, Text, StyleSheet, Image, SafeAreaView, Button, TextInput, TouchableOpacity } from 'react-native';
-import PagerView from 'react-native-pager-view';
-import auth from '@react-native-firebase/auth';
+import React, { useEffect, useRef, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  Button,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import PagerView from "react-native-pager-view";
+import auth from "@react-native-firebase/auth";
 import { useRouter } from "expo-router";
 import { useNavigation } from "@react-navigation/native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import PhoneInput from "react-native-phone-input";
-
-
-
-
 
 export default function Settings() {
   const router = useRouter();
@@ -17,86 +22,85 @@ export default function Settings() {
 
   const handleChangePassword = () => {
     setMessageVisible(true);
-    
 
     //Eventually add code here to send email to users
   };
 
   return (
     <SafeAreaView style={styles.main}>
-
       {/* Header */}
       <View style={styles.container}>
-          <View style={styles.header}>
-            <Image
-              source={require('@/assets/images/1Point_Logo.png')}
-              style={styles.headerImage}
-            />
-            <View style={styles.headerText}>
-              <Text style={styles.welcomeText}>Settings</Text>
-            </View>
+        <View style={styles.header}>
+          <Image
+            source={require("@/assets/images/1Point_Logo.png")}
+            style={styles.headerImage}
+          />
+          <View style={styles.headerText}>
+            <Text style={styles.welcomeText}>Settings</Text>
           </View>
+        </View>
       </View>
-
 
       {/*Settings Panel*/}
       <View style={styles.settingsPanel}>
-
         <View style={styles.row}>
           <View style={styles.pointContainer}>
-          <MaterialCommunityIcons
+            <MaterialCommunityIcons
               style={styles.icon}
               name="account"
               size={24}
               color="black"
-          />
+            />
           </View>
-          <TextInput accessibilityLabel = "name"
-          placeholder = "John Doe"
-          style={styles.input}>            
-          </TextInput>
+          <TextInput
+            accessibilityLabel="name"
+            placeholder="John Doe"
+            style={styles.input}
+          ></TextInput>
         </View>
 
         <View style={styles.row}>
           <View style={styles.pointContainer}>
-          <MaterialCommunityIcons
+            <MaterialCommunityIcons
               style={styles.icon}
               name="email"
               size={24}
               color="black"
-          />
+            />
           </View>
-          <TextInput accessibilityLabel = "email"
-          placeholder = "john.doe@gmail.com"
-          style={styles.input}>            
-          </TextInput>
+          <TextInput
+            accessibilityLabel="email"
+            placeholder="john.doe@gmail.com"
+            style={styles.input}
+          ></TextInput>
         </View>
 
         <View style={styles.row}>
-          <PhoneInput
-            style={styles.input}
-            initialCountry="ca"
-          />
+          <PhoneInput style={styles.input} initialCountry="ca" />
         </View>
 
-        <TouchableOpacity 
-        style={styles.changePassword}
-        accessibilityLabel='change password'
-        onPress = {handleChangePassword}>
+        <TouchableOpacity
+          style={styles.changePassword}
+          accessibilityLabel="change password"
+          onPress={handleChangePassword}
+        >
           <Text style={styles.pointText}>Change Password</Text>
         </TouchableOpacity>
 
         {messageVisible && (
-          <Text style={styles.changeMessage}>An email to change your password has been sent to you!</Text>
+          <Text style={styles.changeMessage}>
+            An email to change your password has been sent to you!
+          </Text>
         )}
 
         <TouchableOpacity
-        style={styles.backButton}
-        accessibilityLabel='log out'
-        onPress={() => router.navigate("/")}>
-          <Text style = {styles.backButtonText}>Log Out</Text>
+          style={styles.backButton}
+          accessibilityLabel="logout"
+          onPress={() => auth().signOut()}
+        >
+          <Text style={styles.backButtonText}>Log Out</Text>
         </TouchableOpacity>
-        </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -105,44 +109,44 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   container: {
     flex: 1,
     padding: 15,
     paddingTop: 40,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   headerText: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginRight: 71,
   },
   header: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 20,
   },
   welcomeText: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   newSection: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#eee',
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#eee",
     padding: 10,
     borderRadius: 32,
     margin: 5,
-    position: 'relative',
+    position: "relative",
   },
   headerImage: {
     width: 71,
     height: 71,
     marginRight: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   pointAmounts: {
     width: 40,
@@ -159,7 +163,7 @@ const styles = StyleSheet.create({
   newText: {
     fontSize: 20,
     marginLeft: 10,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     flexShrink: 1,
   },
   icon: {
@@ -168,55 +172,55 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   settingsPanel: {
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
     padding: 15,
     borderTopLeftRadius: 32,
     borderTopRightRadius: 32,
-    minHeight:650,
+    minHeight: 650,
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: '#fff',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
     padding: 15,
     borderRadius: 32,
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 10,
     marginVertical: 24,
-    minHeight: 75
+    minHeight: 75,
   },
   pointContainer: {
-    flexDirection: 'row',
-    alignItems: 'center'
+    flexDirection: "row",
+    alignItems: "center",
   },
   pointText: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   label: {
     fontSize: 16,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   transactionRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   transactionText: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   page: {
     flex: 1,
     padding: 15,
     paddingTop: 40,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     maxHeight: 200,
   },
   dotsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
     marginVertical: 10,
   },
   dot: {
@@ -226,29 +230,29 @@ const styles = StyleSheet.create({
     marginHorizontal: 5,
   },
   activeDot: {
-    backgroundColor: 'black',
+    backgroundColor: "black",
   },
   inactiveDot: {
-    backgroundColor: 'gray',
-  }, 
+    backgroundColor: "gray",
+  },
   input: {
     flex: 1,
-    fontSize:16
+    fontSize: 16,
   },
   changePassword: {
     borderWidth: 1,
     borderColor: "black",
-    marginTop:60,
+    marginTop: 60,
     padding: 10,
     borderRadius: 10,
     width: "100%",
     alignItems: "center",
   },
-  changeMessage:{
-    marginTop:15,
-    fontSize:18,
-    color:"black",
-    textAlign:"center"
+  changeMessage: {
+    marginTop: 15,
+    fontSize: 18,
+    color: "black",
+    textAlign: "center",
   },
   inputWrapper: {
     flexDirection: "row",
@@ -273,7 +277,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginVertical: 10,
     width: "100%",
-    marginTop:20
+    marginTop: 20,
   },
   backButtonText: {
     color: "white",
